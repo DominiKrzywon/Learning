46c693255cba426ff6366dd813df0fe022e165db
Dominik <dominikkrzywon@gmail.com>
2026-04-05 20:45:09 +0200

    Refactor registration tests to use AuthApi for API calls

--- tests/api/crud/register.spec.ts
+++ tests/api/crud/register.spec.ts
@@ -1,51 +1,50 @@
 import { USER_NAME } from '@_config/env.config';
+import { AuthApi } from '@_src/api/auth.api';
 import { prepareRandomUser } from '@_src/factory/user.factory';
 import { restoreSystem } from '@_src/helper/restore';
 import { RegisterUserModel } from '@_src/models/user.model';
-import { apiUrls } from '@_src/utils/api.util';
 import { HTTP_STATUS } from '@_src/utils/http-status';
 import { expect, test } from '@playwright/test';
 
 test.describe('Registration API', () => {
   let registerUserData: RegisterUserModel;
+  let authApi: AuthApi;
 
   test.beforeEach(async ({ request }) => {
     await restoreSystem(request);
     registerUserData = prepareRandomUser();
+    authApi = new AuthApi(request);
   });
 
-  test('should register a new user @logged', async ({ request }) => {
-    const response = await request.post(apiUrls.registerUrl, {
-      data: registerUserData,
-    });
+  test('should register a new user @logged', async () => {
+    const { resRegister, jsonRegister } =
+      await authApi.register(registerUserData);
 
-    const responseJson = await response.json();
-
-    expect(response.status()).toBe(HTTP_STATUS.OK);
-    expect(responseJson.success).toBe(true);
+    expect(resRegister.status()).toBe(HTTP_STATUS.OK);
+    expect(jsonRegister.success).toBe(true);
   });
 
-  test('should not register with a created username @logged', async ({
-    request,
-  }) => {
+  test('should not register with a created username @logged', async () => {
     registerUserData = prepareRandomUser({ username: USER_NAME });
+    
+    const expectedErrorMessage =
+      'User already exists with that username or email';
+    const { resRegister, jsonRegister } =
+      await authApi.register(registerUserData);
 
-    const response = await request.post(apiUrls.registerUrl, {
-      data: registerUserData,
-    });
-
-    expect(response.status()).toBe(HTTP_STATUS.UNPROCESSABLE);
+    expect(resRegister.status()).toBe(HTTP_STATUS.UNPROCESSABLE);
+    expect(jsonRegister).toHaveProperty('error.message', expectedErrorMessage);
   });
 
-  test('should not register with incorrect data - not email provided @logged', async ({
-    request,
-  }) => {
+  test('should not register with incorrect data - not email provided @logged', async () => {
     registerUserData.username = USER_NAME;
 
-    const response = await request.post(apiUrls.registerUrl, {
-      data: registerUserData,
-    });
+    const expectedErrorMessage =
+      'User already exists with that username or email';
+    const { resRegister, jsonRegister } =
+      await authApi.register(registerUserData);
 
-    expect(response.status()).toBe(HTTP_STATUS.UNPROCESSABLE);
+    expect(resRegister.status()).toBe(HTTP_STATUS.UNPROCESSABLE);
+    expect(jsonRegister).toHaveProperty('error.message', expectedErrorMessage);
   });
 });
