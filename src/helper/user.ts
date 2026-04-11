136332356b6708ebc99816bf954f8b473744f48f
Dominik <dominikkrzywon@gmail.com>
2026-04-11 22:18:35 +0200

    Refactor createUserAndLogin function to improve user registration flow and error handling

--- src/helper/user.ts
+++ src/helper/user.ts
@@ -1,26 +1,33 @@
+import { AuthApi } from '@_src/api/auth.api';
 import { prepareRandomUser } from '@_src/factory/user.factory';
 import { loginAndGetUser } from '@_src/helper/auth';
+import { RegisterUserModel } from '@_src/models/user.model';
 import { TestUser } from '@_src/types/test-user.type';
-import { apiUrls } from '@_src/utils/api.util';
-import { APIRequestContext, APIResponse } from '@playwright/test';
+import { APIRequestContext } from '@playwright/test';
 
 export async function createUserAndLogin(
   request: APIRequestContext,
+  userData?: RegisterUserModel,
 ): Promise<TestUser> {
-  const registerUserData = prepareRandomUser();
-  await request.post(apiUrls.registerUrl, {
-    data: registerUserData,
-  });
+  const finalUserData = userData || prepareRandomUser();
 
-  const loginContext = await loginAndGetUser(request, {
-    username: registerUserData.username,
-    password: registerUserData.password,
-  });
+  const authApi = new AuthApi(request);
+  const { resRegister, jsonRegister } = await authApi.register(finalUserData);
+  if (!resRegister.ok()) {
+    throw new Error('Registration failed!');
+  }
+
+  const credentials = {
+    username: finalUserData.username,
+    password: finalUserData.password,
+  };
+
+  const login = await loginAndGetUser(request, credentials);
 
   return {
-    userId: loginContext.userId,
-    authHeader: loginContext.authHeader,
-    username: registerUserData.username,
-    password: registerUserData.password,
+    userId: login.userId,
+    authHeader: login.authHeader,
+    username: finalUserData.username,
+    password: finalUserData.password,
   };
 }
