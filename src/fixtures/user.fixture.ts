29be954048a86fe8e59bc605dbce3ede5b4c561d
Dominik <dominikkrzywon@gmail.com>
2026-04-19 20:24:26 +0200

    Enhance user fixture setup; generate random username and email for loggedUser and improve user creation process

--- src/fixtures/user.fixture.ts
+++ src/fixtures/user.fixture.ts
@@ -1,10 +1,20 @@
+import { prepareRandomUser } from '@_src/factory/user.factory';
 import { createUserAndLogin } from '@_src/helper/user';
 import { TestUser } from '@_src/types/test-user.type';
 import { test as base } from '@playwright/test';
 
 export const test = base.extend<{ loggedUser: TestUser }>({
-  loggedUser: async ({ request }, use) => {
-    const user = await createUserAndLogin(request);
+  loggedUser: async ({ request }, use, testInfo) => {
+    const id = `${testInfo.workerIndex}_${Date.now()}_${Math.random().toString(10).slice(2, 6)}`;
+    const username = `u${id}`.toLowerCase().slice(0, 20);
+    const email = `${username}@e2e.test`;
+
+    const userData = prepareRandomUser({
+      username,
+      email,
+    });
+
+    const user = await createUserAndLogin(request, userData);
 
     await use(user);
   },
